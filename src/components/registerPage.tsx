a290683be92c8e88c043e7d1f9a7914d3f9d16db
Dilemma98 <emmaah98@outlook.com>
2025-04-02 08:35:50 +0200

    AppIndexAndComponentsFilesFilled

---
+++ src/components/registerPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class RegisterPage extends React.Component{
+    render(){
+        return(
+            <div></div>
+        );
+    }
+}
+export default RegisterPage;
