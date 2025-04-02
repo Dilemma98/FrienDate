c268035ea1d384ee280167af9bb2b46a8b4118fb
Dilemma98 <emmaah98@outlook.com>
2025-04-02 09:42:56 +0200

    Added text to ideaPage

--- src/components/idea.tsx
+++ src/components/idea.tsx
@@ -3,7 +3,16 @@ import React from "react";
 class Idea extends React.Component{
     render(){
         return(
-            <div></div>
+            <div>
+                 <h1 className="componentHeader">Affärsidé</h1>
+                <hr></hr>
+                <p>
+                    FrienDate är tänkt att underlätta för människor att skapa och vårda sina relationer genom att identifiera 
+                    gemensamma luckor i gruppens kalendrar och skicka förslag på väderbaserade aktiviteter när ledig tid 
+                    för alla finns. <br></br>Min förhoppning är att tjänsten gör det enklare att planera och prioritera dessa värdefulla 
+                    möten, så både jag själv och andra kan få ut mer tid med nära och kära.
+                </p>
+            </div>
         );
     }
 }
