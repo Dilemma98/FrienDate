83abfbeaa987fdacca8ab0a8593ef7323352942d
Dilemma98 <emmaah98@outlook.com>
2025-04-07 13:07:32 +0200

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "../src/components/contact";
 import Footer from "../src/components/footer";
 import Idea from "../src/components/idea";
